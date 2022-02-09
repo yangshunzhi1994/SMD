e4ba9ff7e5b6bf257a856d80ecc3ab95f5f5e0d9
yangshunzhi1994 <42691414+yangshunzhi1994@users.noreply.github.com>
2022-02-09 09:39:52 +0800

    Update train_teacher.py

--- train_teacher.py
+++ train_teacher.py
@@ -39,9 +39,7 @@ parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
 parser.add_argument('--direction', default=0, type=float, help='direction')
 parser.add_argument('--variance', default=0, type=float, help='variance')
 parser.add_argument('--num_workers', type=int, default=1, help='num_workers')
-parser.add_argument('--fusion', type=str, default="OurDiversity", 
-                    help='OurDiversity,Average,AEKD,EDM,RIDE,KDCL,AED,ENDD,Few-Shot,EKD')
-                    # OurDiversity,Average,AEKD,EDM,RIDE,KDCL,AED,ENDD,Few-Shot,FFMCD,OKDDip,ONE,PCL
+parser.add_argument('--fusion', type=str, default="OurDiversity", help='OurDiversity')
 args = parser.parse_args()
 
 best_ACC = 0
@@ -148,15 +146,7 @@ net3 = Teacher(num_classes=NUM_CLASSES).cuda()
 net4 = Teacher(num_classes=NUM_CLASSES).cuda()
 criterion = nn.CrossEntropyLoss().cuda()
 
-if args.fusion == 'OKDDip' or args.fusion == 'PCL': 
-    if args.fusion == 'OKDDip':
-        net = other.OKDDip(in_dim=NUM_CLASSES, out_dim=NUM_CLASSES)
-    else:
-        net = other.PCL(out_dim=NUM_CLASSES)
-    net.cuda()
-    optimizer = optim.SGD(itertools.chain(net1.parameters(),net2.parameters(),net3.parameters(),net4.parameters(),net.parameters()), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-else:
-    optimizer = optim.SGD(itertools.chain(net1.parameters(),net2.parameters(),net3.parameters(),net4.parameters()), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+optimizer = optim.SGD(itertools.chain(net1.parameters(),net2.parameters(),net3.parameters(),net4.parameters()), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 
 # Training
 def train(epoch):
@@ -165,10 +155,6 @@ def train(epoch):
     net2.train()
     net3.train()
     net4.train()
-    if args.fusion == 'OKDDip' or args.fusion == 'PCL': 
-        net.train()
-    else:
-        pass
     train_loss = 0
 
     conf_mat = np.zeros((NUM_CLASSES, NUM_CLASSES))
@@ -203,90 +189,11 @@ def train(epoch):
         loss3 = criterion(outputs3, targets)
         loss4 = criterion(outputs4, targets)
         
-        if args.fusion == 'Average':
-            mimic = (outputs1+outputs2+outputs3+outputs4)/4
-            loss = criterion(mimic, targets)
-            loss = loss + loss1 + loss2 + loss3 + loss4 
-        elif args.fusion == 'AEKD':
-            mimic = other.AEKD_Teacher().cuda()(outputs1,outputs2,outputs3,outputs4)
-            loss = criterion(mimic, targets)
-            loss = loss + loss1 + loss2 + loss3 + loss4 
-        elif args.fusion == 'OurDiversity':
+        if args.fusion == 'OurDiversity':
             mimic = (outputs1+outputs2+outputs3+outputs4)/4
             loss = criterion(mimic, targets)
             diversityLoss = losses.Diversity(args.direction, args.variance).cuda()(outputs1,outputs2,outputs3,outputs4,targets)
             loss = loss + loss1 + loss2 + loss3 + loss4 + diversityLoss
-        elif args.fusion == 'EDM':# Protecting DNNs from Theft using an Ensemble of Diverse Models 
-            emd_loss = other.EDM().cuda()(outputs1,outputs2,outputs3,outputs4)
-            mimic = (outputs1+outputs2+outputs3+outputs4)/4
-            loss = 0.25*loss1 + 0.25*loss2 + 0.25*loss3 + 0.25*loss4 + emd_loss
-        elif args.fusion == 'RIDE':# RIDE: Long-tailed Recognition by Routing Diverse Distribution-Aware Experts.
-            ride_loss = other.RIDE().cuda()(outputs1,outputs2,outputs3,outputs4,targets)
-            mimic = (outputs1+outputs2+outputs3+outputs4)/4
-            loss = loss1 + loss2 + loss3 + loss4 - 0.2*ride_loss
-        elif args.fusion == 'KDCL':# Online Knowledge Distillation via Collaborative Learning
-            kdcl_loss, mimic = other.KDCL().cuda()(outputs1,outputs2,outputs3,outputs4,targets)
-            loss = loss1 + loss2 + loss3 + loss4 + kdcl_loss
-        elif args.fusion == 'AED':# Adaptable Ensemble Distillation
-            loss, mimic = other.AED().cuda()(outputs1,outputs2,outputs3,outputs4,targets)
-        elif args.fusion == 'ENDD':# Ensemble Distribution Distillation
-            outputs1 = torch.exp(outputs1)
-            outputs2 = torch.exp(outputs2)
-            outputs3 = torch.exp(outputs3)
-            outputs4 = torch.exp(outputs4)
-            mimic = (outputs1+outputs2+outputs3+outputs4)/4
-            loss = criterion(mimic, targets)
-            loss = loss1 + loss2 + loss3 + loss4 + loss
-        elif args.fusion == 'Few-Shot':# Diversity with Cooperation: Ensemble Methods for Few-Shot Classification
-            mimic = (outputs1+outputs2+outputs3+outputs4)/4
-            loss = other.FewShot().cuda()(outputs1,outputs2,outputs3,outputs4,targets)
-            loss = loss1 + loss2 + loss3 + loss4 + loss
-        elif args.fusion == 'FFMCD':#Online Knowledge Distillation via Multi-branch Diversity Enhancement
-            loss_CD = other.CD().cuda()(net1,net2,net3,net4)
-            loss = other.FFMCD().cuda()(outputs1,outputs2,outputs3,outputs4,targets)
-            mimic = (outputs1+outputs2+outputs3+outputs4)/4
-            loss = loss + 0.00000005*loss_CD
-        elif args.fusion == 'OKDDip':#Online Knowledge Distillation with Diverse Peers
-            L_dis = net(outputs1,outputs2,outputs3,outputs4)
-            loss1 = criterion(outputs1, targets)
-            loss2 = criterion(outputs2, targets)
-            loss3 = criterion(outputs3, targets)
-            loss4 = criterion(outputs4, targets)
-            loss = loss1 + loss2 + loss3 + loss4 + L_dis
-            mimic = outputs4
-        elif args.fusion == 'ONE':# Knowledge Distillation by On-the-Fly Native Ensemble
-            loss1 = criterion(outputs1, targets)
-            loss2 = criterion(outputs2, targets)
-            loss3 = criterion(outputs3, targets)
-            loss4 = criterion(outputs4, targets)
-            
-            mimic = torch.cat((outputs1.unsqueeze(0), outputs2.unsqueeze(0), outputs3.unsqueeze(0), outputs4.unsqueeze(0)),0)
-            g = F.softmax(mimic,dim=0)
-            mimic = g[0]*outputs1 + g[1]*outputs2 + g[2]*outputs3 + g[3]*outputs4
-            mimic_loss = criterion(mimic, targets)
-            
-            kl_loss1 = other.KL_divergence(temperature = 3).cuda()(mimic,outputs1)
-            kl_loss2 = other.KL_divergence(temperature = 3).cuda()(mimic,outputs2)
-            kl_loss3 = other.KL_divergence(temperature = 3).cuda()(mimic,outputs3)
-            kl_loss4 = other.KL_divergence(temperature = 3).cuda()(mimic,outputs4)
-            loss = loss1+loss2+loss3+loss4+mimic_loss+kl_loss1+kl_loss2+kl_loss3+kl_loss4
-        elif args.fusion == 'PCL':# Peer Collaborative Learning for Online Knowledge Distillation
-            mimic = net(mimic1,mimic2,mimic3,mimic4)
-            loss1 = criterion(outputs1, targets)
-            loss2 = criterion(outputs2, targets)
-            loss3 = criterion(outputs3, targets)
-            loss4 = criterion(outputs4, targets)
-            mimic_loss = criterion(mimic, targets)
-            kl_loss1 = other.sigmoid_rampup(epoch, 80)*other.KL_divergence(temperature = 3).cuda()(mimic,outputs1)
-            kl_loss2 = other.sigmoid_rampup(epoch, 80)*other.KL_divergence(temperature = 3).cuda()(mimic,outputs2)
-            kl_loss3 = other.sigmoid_rampup(epoch, 80)*other.KL_divergence(temperature = 3).cuda()(mimic,outputs3)
-            kl_loss4 = other.sigmoid_rampup(epoch, 80)*other.KL_divergence(temperature = 3).cuda()(mimic,outputs4)
-            loss = loss1+loss2+loss3+loss4+mimic_loss+kl_loss1+kl_loss2+kl_loss3+kl_loss4
-        elif args.fusion == 'EKD':#Ensemble Knowledge Distillation for Learning Improved and Efficient Networks
-            mimic = outputs1+outputs2+outputs3+outputs4
-            tea_cls_loss = criterion(mimic, targets)
-            EKD_loss = other.EKD_Teacher().cuda()(outputs1,outputs2,outputs3,outputs4,mimic)
-            loss = 0.5*tea_cls_loss+0.6*EKD_loss
         else:
             raise Exception('Invalid ...')
         
@@ -309,11 +216,6 @@ def test(epoch):
     net2.eval()
     net3.eval()
     net4.eval()
-    
-    if args.fusion == 'OKDDip' or args.fusion == 'PCL': 
-        net.eval()
-    else:
-        pass
     
     conf_mat = np.zeros((NUM_CLASSES, NUM_CLASSES))
     conf_mat1 = np.zeros((NUM_CLASSES, NUM_CLASSES))
@@ -341,30 +243,7 @@ def test(epoch):
         outputs3 = outputs3.view(test_bs, ncrops, -1).mean(1) 
         outputs4 = outputs4.view(test_bs, ncrops, -1).mean(1)
         
-        if args.fusion == 'AEKD':
-            mimic = other.AEKD_Teacher().cuda()(outputs1,outputs2,outputs3,outputs4)
-        elif args.fusion == 'FFMCD':
-            mimic = torch.cat((outputs1.unsqueeze(0),outputs2.unsqueeze(0),outputs3.unsqueeze(0),outputs4.unsqueeze(0)),0)
-            mimic = F.softmax(mimic,dim=2)
-            mimic = mimic[0]*outputs1+mimic[1]*outputs2+mimic[2]*outputs3+mimic[3]*outputs4
-        elif args.fusion == 'ENDD':
-            outputs1 = torch.exp(outputs1)
-            outputs2 = torch.exp(outputs2)
-            outputs3 = torch.exp(outputs3)
-            outputs4 = torch.exp(outputs4)
-            mimic = (outputs1+outputs2+outputs3+outputs4)/4
-        elif args.fusion == 'ONE':# Knowledge Distillation by On-the-Fly Native Ensemble
-            mimic = torch.cat((outputs1.unsqueeze(0), outputs2.unsqueeze(0), outputs3.unsqueeze(0), outputs4.unsqueeze(0)),0)
-            g = F.softmax(mimic,dim=0)
-            mimic = g[0]*outputs1 + g[1]*outputs2 + g[2]*outputs3 + g[3]*outputs4
-        elif args.fusion == 'PCL':# Peer Collaborative Learning for Online Knowledge Distillation
-            mimic1 = mimic1.view(test_bs, ncrops, -1).mean(1)
-            mimic2 = mimic2.view(test_bs, ncrops, -1).mean(1)
-            mimic3 = mimic3.view(test_bs, ncrops, -1).mean(1) 
-            mimic4 = mimic4.view(test_bs, ncrops, -1).mean(1)
-            mimic = net(mimic1,mimic2,mimic3,mimic4)
-        else:
-            mimic = (outputs1+outputs2+outputs3+outputs4)/4
+        mimic = (outputs1+outputs2+outputs3+outputs4)/4
             
         loss = criterion(mimic, targets)
         PrivateTest_loss += loss.item()
